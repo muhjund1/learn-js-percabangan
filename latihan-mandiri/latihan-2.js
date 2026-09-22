2f2195d464400501ad2b8dcb14ca462e7abce5da
Muhammad Jundi Al-Aqsha <muhammadjunnndi@gmail.com>
2026-09-22 07:44:25 +0700

    latihan 2

---
+++ latihan-mandiri/latihan-2.js
@@ -0,0 +1,15 @@
+let parkir = "Mobil";
+
+switch (parkir) {
+    case "Mobil":
+        console.log("Biaya parkir = 5000");
+        break;
+
+    case "Motor":
+        console.log("Biaya parkir = 2000");
+        break;
+
+    case "Truk":
+        console.log("Biaya parkir = 10000");
+        break
+}
