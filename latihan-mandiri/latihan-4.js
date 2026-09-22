800726e3096a897392de4dea1e0088e8e1bfd68d
Muhammad Jundi Al-Aqsha <muhammadjunnndi@gmail.com>
2026-09-22 08:07:44 +0700

    latihan 4

---
+++ latihan-mandiri/latihan-4.js
@@ -0,0 +1,8 @@
+let email = "juned@gmail.com";
+let password = "junaidi89";
+
+if (password.length < 8) {
+    console.log("Password minimal 8 karakter");
+} else {
+    console.log("Baik password Anda sudah baik & benar");
+}
