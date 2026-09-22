689e35b4da2e25c4517726277d56d7442467bcbd
Muhammad Jundi Al-Aqsha <muhammadjunnndi@gmail.com>
2026-09-22 08:24:02 +0700

    perbaikan latihan 4

--- latihan-mandiri/latihan-4.js
+++ latihan-mandiri/latihan-4.js
@@ -4,5 +4,5 @@ let password = "junaidi89";
 if (password.length < 8) {
     console.log("Password minimal 8 karakter");
 } else {
-    console.log("Baik password Anda sudah baik & benar");
+    console.log("Baik password  sudah baik & benar");
 }
